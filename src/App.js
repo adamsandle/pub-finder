5fb546e22d4489c73e539528e40df97c3ebb5432
Adam Sandle <18138428+adamsandle@users.noreply.github.com>
2018-10-10 20:45:53 +0100

    Move the header into its own component (#2)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Radar from "./components/radar";
-import logo from "./logo.svg";
+import Header from "./components/header";
 import "./App.css";
 
 import { updatePosition, fetchLocations } from "./actions/actions";
@@ -35,10 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+
+export default () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+  );
+};
